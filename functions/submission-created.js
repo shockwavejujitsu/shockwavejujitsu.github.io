fdd3b693cd011defc3f3a4929f134a9c754316b8
Adam <adam@adamcrowe.com>
2019-11-03 18:24:04 +0000

    Add netlify function

---
+++ functions/submission-created.js
@@ -0,0 +1,24 @@
+'use strict';
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export function handler(event, context, callback) {
+    console.log('+++ event: ', event);
+
+    let inboxEmail = 'shockwavejujitsu@gmail.com'
+    let formData = JSON.parse(event.body).data;
+
+    let html = `<h1>Form submission from ${formData.name}</h1>
+                <p><strong>Email Address:</strong> ${formData.email}</p>
+                <p><strong>Message:</strong> ${formData.message}</p>`;
+
+    const msg = {
+        to: inboxEmail,
+        from: formData.email,
+        subject: 'Website Contact Form Submission',
+        text: formData.message,
+        html: html,
+    };
+
+    sgMail.send(msg);
+}
